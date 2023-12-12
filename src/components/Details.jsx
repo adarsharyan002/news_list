0ff466a5d62a6d7d6569b5da5e380a182c7220aa
adarsharyan002 <adarsharyanmuz@gmail.com>
2023-12-12 22:18:48 +0530

    added loading screen

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,64 +1,57 @@
-
-
-
-import  {useEffect,useState} from "react"
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
-
 const Details = () => {
+  const [itemDetails, setItemDetails] = useState(null);
+  const [comments, setComments] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
 
+  useEffect(() => {
+    const fetchNews = async () => {
+      const url = `http://hn.algolia.com/api/v1/items/${id}`;
+      try {
+        const response = await axios.get(url);
+        const data = response.data;
 
-     const [itemDetails,setItemDetails]=useState(null)
-     const [comments,setComments]=useState(null)
-     const {id} = useParams();
+        setItemDetails(data);
+        setComments(data?.children.slice(0, 10));
+        setIsLoading(false); // Set loading to false when data arrives
 
-     console.log(comments)
+      } catch (error) {
+        console.error('Error fetching news:', error);
+      }
+    };
 
-    useEffect(() => {
-       
-        const fetchNews = async () => {
-            const url = `http://hn.algolia.com/api/v1/items/${id}`;
-            try {
-              const response = await axios.get(url);
-              const data = response.data;
-              // Uncomment the line below to limit the number of items in your response.
-              // data.hits.length = 10;
-             
-             setItemDetails(data);
-             setComments(data?.children.slice(0, 10));
+    fetchNews();
+  }, [id]);
 
-            } catch (error) {
-              // Handle errors here
-              console.error('Error fetching news:', error);
-            }
-          };
-    
-        fetchNews()
-        
-      }, [id])
-      return (
-        <>
-          {itemDetails &&
-            <div className="p-4 border border-gray-300 rounded mb-4">
-              <p className="text-xl font-bold text-teal-500">{itemDetails.points}</p>
-              <p className="text-lg">{itemDetails.title}</p>
-            </div>
-          }
-      
-          <div>
-            <h1 className="text-2xl font-bold mb-4 m-2 text-teal-500">All comments</h1>
-            <ul className="list-disc pl-4 m-3">
-            {comments?.map((comment, i) => {
-              return (
-                <li key={i} className="mb-2">{comment.text}</li>
-              );
-            })}
-            </ul>
-          </div>
-        </>
-      );
-      
-}
- 
+  if (isLoading) {
+    return <div>Loading...</div>; // Render loading indicator
+  }
+
+  return (
+    <>
+      {itemDetails &&
+        <div className="p-4 border border-gray-300 rounded mb-4">
+          <p className="text-xl font-bold text-teal-500">{itemDetails.points}</p>
+          <p className="text-lg">{itemDetails.title}</p>
+        </div>
+      }
+
+      <div>
+        <h1 className="text-2xl font-bold mb-4 m-2 text-teal-500">All comments</h1>
+        <ul className="list-disc pl-4 m-3">
+          {comments?.map((comment, i) => {
+            return (
+              <li key={i} className="mb-2">{comment.text}</li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
+
 export default Details;
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,52 +1,58 @@
-
-import { format } from "date-fns"
+import { useState, useEffect } from "react";
+import { format } from "date-fns";
 import { Link } from "react-router-dom";
 
+const Posts = ({ largeTitle, query, items }) => {
+  const [loading, setLoading] = useState(true);
 
-const Posts = ({largeTitle,query,items}) => {
+  useEffect(() => {
+    // Update loading state when items are present
+    if (items.length > 0) {
+      setLoading(false);
+    }
+  }, [items]);
 
-    
-    return ( 
-        <div className="bg-white">
-         
+  return (
+    <div className="bg-white">
+      <article className="container mx-auto my-3 lg:max-w-4xl px-5">
+        <p className="text-gray-600">
+          Category:{" "}
+          <span className="font-bold text-gray-400 capitalize">{query}</span>
+        </p>
+      </article>
 
-            <article className="container mx-auto my-3 lg:max-w-4xl px-5">
-              <p className="text-gray-600">
-                Category:{" "}
-                <span className="font-bold text-gray-400 capitalize">
-                  {query}
-                </span>
-              </p>
-            </article>
+      <section className="grid grid-cols-1 gap-5 p-5 md:grid-cols-2 container mx-auto lg:max-w-4xl">
+        {loading ? (
+          <div className="text-center">Loading...</div>
+        ) : (
+          items.map((item) => {
+            const { author, created_at, objectID, title } = item;
 
-            <section className="grid grid-cols-1 gap-5 p-5 md:grid-cols-2 container mx-auto lg:max-w-4xl">
-              {items.map((item) => {
-                const { author, created_at, objectID, title } = item
+            return (
+              <article
+                key={objectID}
+                className="bg-slate-100 shadow-md rounded p-3 transition-all duration-150"
+              >
+                <h3 className="font-semibold text-teal-500 text-xl mb-3">
+                  {title}
+                </h3>
+                <article className="flex items-center justify-between">
+                  <p className="text-gray-600">
+                    By <em>{author}</em>
+                  </p>
+                  <Link to={`/details/${objectID}`}>Read more...</Link>
+                </article>
+                <p className="text-gray-400 mt-10">
+                  {/* Format date using the `format` method from `date-fns` */}
+                  {format(new Date(created_at), "dd MMM yyyy")}
+                </p>
+              </article>
+            );
+          })
+        )}
+      </section>
+    </div>
+  );
+};
 
-                return (
-                  <article
-                    key={objectID}
-                    className="bg-slate-100 shadow-md rounded p-3 transition-all duration-150"
-                  >
-                    <h3 className="font-semibold text-teal-500 text-xl mb-3">
-                      {title}
-                    </h3>
-                    <article className="flex items-center justify-between">
-                      <p className="text-gray-600">
-                        By <em>{author}</em>
-                      </p>
-                      <Link to={`/details/${objectID}`}>Read more...</Link>
-                    </article>
-                    <p className="text-gray-400 mt-10">
-                      {/* Format date using the `format` method from `date-fns` */}
-                      {format(new Date(created_at), "dd MMM yyyy")}
-                    </p>
-                  </article>
-                )
-              })}
-            </section>
-        </div>
-     );
-}
- 
 export default Posts;
